7898d9aca4ed924360682c0e952dd9d0e60d756b
Bernardo Laing <bernardo.laing@gmail.com>
2022-01-17 21:48:18 -0600

    (BE) Refactor auth into module

---
+++ backend/app/app/api/deps.py
@@ -0,0 +1,65 @@
+from typing import Generator
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+from pydantic import ValidationError
+from sqlalchemy.orm import Session
+
+from app.core import security
+from app.core.config import settings
+from app.db.session import SessionLocal
+from app.modules.auth import (
+    repositories as auth_repositories,
+    models as auth_models,
+    schemas as auth_schemas
+)
+
+reusable_oauth2 = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_V1_STR}/login/access-token"
+)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+def get_current_user(
+    db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)
+) -> auth_models.User:
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+        )
+        token_data = auth_schemas.TokenPayload(**payload)
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
+    user = auth_repositories.user.get(db, id=token_data.sub)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+def get_current_active_user(
+    current_user: auth_models.User = Depends(get_current_user),
+) -> auth_models.User:
+    if not auth_repositories.user.is_active(current_user):
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+def get_current_active_superuser(
+    current_user: auth_models.User = Depends(get_current_user),
+) -> auth_models.User:
+    if not auth_repositories.user.is_superuser(current_user):
+        raise HTTPException(
+            status_code=400, detail="The user doesn't have enough privileges"
+        )
+    return current_user

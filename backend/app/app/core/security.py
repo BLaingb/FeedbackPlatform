88e7e5f803cc6a0286cdd7a069a7e913f5dd7e96
Bernardo Laing <bernardo.laing@gmail.com>
2022-01-19 08:03:36 -0600

    [BE] Add permissions to JWT

--- backend/app/app/core/security.py
+++ backend/app/app/core/security.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timedelta
-from typing import Any, Union
+from typing import Any, List, Union
 
 from jose import jwt
 from passlib.context import CryptContext
 
 from app.core.config import settings
+from app.modules.auth.models.user import User
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -13,7 +14,7 @@ ALGORITHM = "HS256"
 
 
 def create_access_token(
-    subject: Union[str, Any], expires_delta: timedelta = None
+    subject: Union[str, Any], expires_delta: timedelta = None, **kwargs
 ) -> str:
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -21,7 +22,7 @@ def create_access_token(
         expire = datetime.utcnow() + timedelta(
             minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
         )
-    to_encode = {"exp": expire, "sub": str(subject)}
+    to_encode = {"exp": expire, "sub": str(subject), **kwargs}
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
@@ -32,3 +33,7 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
+
+def check_permissions(user: User, permissions: List[str]):
+    print(user.roles)
--- backend/app/app/modules/auth/api/v1/endpoints/login.py
+++ backend/app/app/modules/auth/api/v1/endpoints/login.py
@@ -34,10 +34,13 @@ def login_access_token(
         raise HTTPException(status_code=400, detail="Incorrect email or password")
     elif not repositories.user.is_active(user):
         raise HTTPException(status_code=400, detail="Inactive user")
+
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     return {
         "access_token": security.create_access_token(
-            user.id, expires_delta=access_token_expires
+            user.id,
+            expires_delta=access_token_expires,
+            permissions=user.get_permissions()
         ),
         "token_type": "bearer",
     }
